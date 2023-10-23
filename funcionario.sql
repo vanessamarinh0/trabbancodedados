9b8b977dddce9debf7759eef63fc21f9d3a6f37d
Alana Lopes <112038055+AlanaLopes@users.noreply.github.com>
2023-10-22 22:28:19 -0300

    Create funcionario.sql

---
+++ funcionario.sql
@@ -0,0 +1,11 @@
+INSERT INTO `funcionario` (`nome_funcionario`, `turno_funcionario`) VALUES
+('Pedro Almeida', 'Noite'),
+('Mariana Costa', 'Noite'),
+('Ana Pereira', 'Manhã'),
+('João Silva', 'Manhã'),
+('Maria Santos', 'Tarde'),
+('Fernando Souza', 'Noite'),
+('Carlos Gomes', 'Tarde'),
+('Luiz Oliveira', 'Manhã'),
+('Isabela Rodrigues', 'Tarde'),
+('Larissa Carvalho', 'Manhã');
