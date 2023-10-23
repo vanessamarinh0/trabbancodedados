8f2fdb02bfe3a027b7b6272dae8ceebb918e1e11
Alana Lopes <112038055+AlanaLopes@users.noreply.github.com>
2023-10-23 20:31:18 -0300

    Update tabela.php

--- tabela.php
+++ tabela.php
@@ -1,3 +1,4 @@
+<?php include 'header.php';?>
 <?php
 include 'listar.php';
 include 'conexao.php';
@@ -10,7 +11,9 @@ include 'conexao.php';
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body>
+<body style="background-image: url('imagem2.jpeg'); background-size: cover; min-height: 100%;
+background-repeat: no-repeat;
+background-position: center center;">
 	<br><br><br>
 	<div class="container col-md-6 offset-md-3">
 		<table  class="table  table-hover">
