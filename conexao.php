51e67f64dda7c179ecbdeb02250c6ad30dbff1cd
Alana Lopes <112038055+AlanaLopes@users.noreply.github.com>
2023-10-22 22:21:55 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$servidor = "localhost" ;
+$usuario = "root" ;
+$senha = "";
+$banco_de_dados = "trabalho";
+
+$conexao =  mysqli_connect($servidor, $usuario, $senha, $banco_de_dados);
+/* MYSQLI CONNECT - ele conecta ao banco */
+
+?>
