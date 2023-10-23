aacd15292886df76e50ffba5218cfdd556d8db3f
Alana Lopes <112038055+AlanaLopes@users.noreply.github.com>
2023-10-22 22:27:26 -0300

    Create cliente.sql

---
+++ cliente.sql
@@ -0,0 +1,11 @@
+INSERT INTO `cliente` (`cpf_cliente`, `endereco_cliente`, `nome_cliente`) VALUES
+('12345678900', 'Rua Exemplo, 123', 'Janaina'),
+('98765432101', 'Avenida Principal, 456', 'Alberto'),
+('55555555555', 'Travessa do Centro, 789', 'Carlos'),
+('11122233344', 'Rua da Amostra, 789', 'Daiana'),
+('44433322211', 'Avenida Central, 987', 'Rosa'),
+('99988877766', 'Rua da Simulação, 567', 'Rafaela'),
+('77788899900', 'Avenida Modelo, 345', 'Mario'),
+('22233344455', 'Travessa Abstrata, 123', 'Claudio'),
+('66677788899', 'Rua Genérica, 789', 'Marcia'),
+('33344455566', 'Avenida Imaginária, 234', 'Sandra')
